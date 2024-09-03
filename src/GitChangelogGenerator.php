f3b2d96d517c2aaf68a7ac1b68860b3885b72130
Alessandro Podo <alessandro.podo@lrz.de>
2024-09-03 09:45:03 +0200

    chore: add editorconfig and gitattributes

--- src/GitChangelogGenerator.php
+++ src/GitChangelogGenerator.php
@@ -20,7 +20,6 @@ use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Twig\Environment;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
